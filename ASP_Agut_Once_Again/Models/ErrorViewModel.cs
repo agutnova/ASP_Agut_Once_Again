e76727eb33f4177a7ac937bb14c498d226698b33
agutnova <agutnova@yandex.ru>
2024-02-19 23:20:56 +0300

    Добавьте файлы проекта.

---
+++ ASP_Agut_Once_Again/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Agut_Once_Again.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
